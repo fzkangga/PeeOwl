de8b18d84cd71f4cc382b85ac31ac11e25045961
Aryne Neil Cabatan <aryneneilcabatan@gmail.com>
2013-10-07 22:04:35 +0800

    FIRST ATTEMPT TO COMMIT

---
+++ gen/com/aryne/PeeOwl/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package com.aryne.PeeOwl;
+
+/* This stub is for using by IDE only. It is NOT the Manifest class actually packed into APK */
+public final class Manifest {
+}
---
+++ gen/com/aryne/PeeOwl/R.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package com.aryne.PeeOwl;
+
+/* This stub is for using by IDE only. It is NOT the R class actually packed into APK */
+public final class R {
+}
